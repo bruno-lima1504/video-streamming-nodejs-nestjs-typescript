9f4009c535d8209aaff8bf1f70b4c4c67ee1bbb0
bruno-lima5104 <brunolima1504@gmail.com>
2026-05-14 18:15:44 -0300

    refactor: Code decoupling and edge validation with DTO

--- src/core/service/midia-player.service.ts
+++ src/core/service/midia-player.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import { VideoNotFoundException } from '../exception/video-not-found-exception';
 
 @Injectable()
 export class MidiaPlayerService {
@@ -9,6 +10,10 @@ export class MidiaPlayerService {
     const video = await this.prismaService.video.findUnique({
       where: { id: videoId },
     });
+
+    if (!video) {
+      throw new VideoNotFoundException(`video with id ${videoId} not found`);
+    }
 
     return video?.url;
   }
--- src/http/rest/contoller/content.controller.ts
+++ src/http/rest/contoller/content.controller.ts
@@ -22,6 +22,9 @@ import fs from 'fs';
 import type { Request, Response } from 'express';
 import { ContentManagementService } from '@src/core/service/content-management.service';
 import { MidiaPlayerService } from '@src/core/service/midia-player.service';
+import { RestResponseInterceptor } from '../interceptor/rest-response.interceptor';
+import { CreateVideoResponseDto } from '../dto/response/create-video-response-ddto';
+import { VideoNotFoundException } from '@src/core/exception/video-not-found-exception';
 
 @Controller('content')
 export class ContentController {
@@ -63,6 +66,7 @@ export class ContentController {
       },
     ),
   )
+  @UseInterceptors(new RestResponseInterceptor(CreateVideoResponseDto))
   async uploadVideo(
     @Req() _req: Request,
     @Body()
@@ -85,13 +89,32 @@ export class ContentController {
       );
     }
 
-    return await this.contentManagementService.createContent({
+    return this.contentManagementService.createContent({
       title: contentData.title,
       description: contentData.description,
       url: videoFile.path,
       thumbnailUrl: thumbnailFile.path,
       sizeInKb: videoFile.size,
     });
+
+    // o interceptor está fazendo essa função abaixo para validar o retorno de apis, com o interceptor não precisamos respetir essa mesma verificação em todas as rotas.
+
+    //aqui pegamos apenas os objetos que pertencem ao DTO e salvamos no obj(exclued...: true)
+    // const transformedObj = plainToInstance(CreateVideoResponseDto, response, {
+    //   excludeExtraneousValues: true,
+    // });
+
+    // e aqui sim fazemos a validação do objeto que a api deve retornar caso esteja difrente do dto ele lança um erro
+    // const errors: ValidationError[] = await validate(transformedObj);
+
+    // if (errors.length > 0) {
+    //   throw new BadRequestException({
+    //     message: 'Response validation failed',
+    //     errors,
+    //   });
+    // }
+
+    // return transformedObj;
   }
 
   @Get('stream/:videoId')
@@ -101,36 +124,48 @@ export class ContentController {
     @Req() req: Request,
     @Res() res: Response,
   ): Promise<any> {
-    const url = await this.midiaPlayerService.PrepareStreaming(videoId);
+    try {
+      const url = await this.midiaPlayerService.PrepareStreaming(videoId);
 
-    if (!url) {
-      throw new NotFoundException('Video not found');
+      if (!url) {
+        throw new NotFoundException('Video not found');
+      }
+
+      const videoPath = path.join('.', url);
+      const fileSize = fs.statSync(videoPath).size;
+
+      // aqui é chave para partirmos o stream de onde paramos
+      const range = req.headers.range;
+      if (range) {
+        const parts = range.replace(/bytes=/, '').split('-');
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunkSize = end - start + 1;
+
+        const file = fs.createReadStream(videoPath, { start, end });
+        res.writeHead(HttpStatus.PARTIAL_CONTENT, {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunkSize,
+          'Content-Type': 'video/mp4',
+        });
+        return file.pipe(res);
+      } else {
+        res.writeHead(HttpStatus.OK, {
+          'Content-Length': fileSize,
+          'Content-Type': 'video/mp4',
+        });
+        fs.createReadStream(videoPath).pipe(res);
+      }
+    } catch (error) {
+      if (error instanceof VideoNotFoundException) {
+        return res.status(HttpStatus.NOT_FOUND).send({
+          message: error.message,
+          error: 'Not Found',
+          statusCode: HttpStatus.NOT_FOUND,
+        });
+      }
+      throw error;
     }
-
-    const videoPath = path.join('.', url);
-    const fileSize = fs.statSync(videoPath).size;
-
-    // aqui é chave para partirmos o stream de onde paramos
-    const range = req.headers.range;
-    if (range) {
-      const parts = range.replace(/bytes=/, '').split('-');
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-      const chunkSize = end - start + 1;
-
-      const file = fs.createReadStream(videoPath, { start, end });
-      res.writeHead(HttpStatus.PARTIAL_CONTENT, {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunkSize,
-        'Content-Type': 'video/mp4',
-      });
-      return file.pipe(res);
-    }
-
-    res.writeHead(HttpStatus.OK, {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    });
   }
 }
