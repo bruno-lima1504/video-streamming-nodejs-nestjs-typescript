b05f9601e34d806fc0c6e0a4064539bbad685f24
bruno-lima5104 <brunolima1504@gmail.com>
2026-05-14 22:24:30 -0300

    feat: Add domain entities

--- src/core/service/content-management.service.ts
+++ src/core/service/content-management.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@src/persistence/prisma/prisma.service';
-import { randomUUID } from 'crypto';
+import { VideoDAO } from '../dao/video.dao';
+import { ContentEntity, ContentType } from '../entity/content.entity';
+import { MovieEntity } from '../entity/movie.entity';
+import { VideoEntity } from '../entity/video.entity';
+import { ThumbnailEntity } from '../entity/thumbnail.entity';
 
 export interface CreateContentData {
   title: string;
@@ -12,25 +15,26 @@ export interface CreateContentData {
 
 @Injectable()
 export class ContentManagementService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly videoDAO: VideoDAO) {}
 
   async createContent(createContentData: CreateContentData) {
-    const { title, description, url, thumbnailUrl, sizeInKb } =
-      createContentData;
-
-    const createVideo = await this.prismaService.video.create({
-      data: {
-        id: randomUUID(),
-        title,
-        description,
-        url,
-        thumbnailUrl,
-        sizeInKb,
-        duration: 100,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
+    const content = ContentEntity.createNew({
+      title: createContentData.title,
+      description: createContentData.title,
+      type: ContentType.MOVIE,
+      media: MovieEntity.createNew({
+        video: VideoEntity.createNew({
+          url: createContentData.url,
+          sizeInKb: createContentData.sizeInKb,
+          duration: 100,
+        }),
+        thumbnail: ThumbnailEntity.createNew({
+          url: createContentData.thumbnailUrl,
+        }),
+      }),
     });
+
+    //const createVideo = await this.videoDAO.create(createContentData);
     return createVideo;
   }
 }
