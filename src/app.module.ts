3b720ceeba78f810bd25ab35fc283dc187470910
bruno-lima5104 <brunolima1504@gmail.com>
2026-05-22 16:36:59 -0300

    chore: wire modular architecture and update tooling
    Register content and identity modules with GraphQL, add module path
    aliases, database scripts and dependencies for the new structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,17 @@
+import { ContentModule } from '@contentModule/content.module';
 import { Module } from '@nestjs/common';
-import { VideoUploadController } from '@src/http/rest/contoller/video-upload..controller';
-import { MidiaPlayerService } from '@src/core/service/midia-player.service';
-import { ContentManagementService } from '@src/core/service/content-management.service';
-import { ContentRepository } from '@src/persistence/repository/content.repository';
-import { VideoRepository } from '@src/persistence/repository/video.repository';
-import { MidiaPlayerController } from '@src/http/rest/contoller/media-player.controller';
-import { PersistenceModule } from '@src/persistence/persistence.module';
-import { externalMovieClient } from '@src/http/rest/client/external-movie-rating/external-movie-rating.client';
-import { HttpClient } from '@src/infra/http/client/http.client';
-import { ConfigModule } from './infra/module/config/config.module';
+import { IdentityModule } from './module/identity/identity.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 @Module({
-  imports: [PersistenceModule.forRoot(), ConfigModule.forRoot()],
-  controllers: [VideoUploadController, MidiaPlayerController],
-  providers: [
-    ContentManagementService,
-    MidiaPlayerService,
-    ContentRepository,
-    VideoRepository,
-    externalMovieClient,
-    HttpClient,
+  imports: [
+    ContentModule,
+    IdentityModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
   ],
 })
 export class AppModule {}
